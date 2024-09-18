9f07ad8fd99b80b99440210b6bbc40bda510d3f8
Ethan Holz <ethan.holz@thoriumworks.com>
2024-09-18 10:52:47 -0500

    refactor: use a simpler way for managing versions

---
+++ default.nix
@@ -0,0 +1,38 @@
+{
+  pkgs ? import <nixpkgs> {},
+  system ? builtins.currentSystem,
+}: let
+  inherit (pkgs) lib;
+  sources = builtins.fromJSON (builtins.readFile ./superhtml.json);
+  mkBinaryInstall = {
+    url,
+    version,
+    hash,
+    downloaded-system
+  }:
+    pkgs.stdenvNoCC.mkDerivation {
+      name = "superhtml-${version}";
+      inherit version;
+      src = pkgs.fetchurl {
+        inherit url;
+        sha256 = hash;
+      };
+      unpackPhase = ''
+        tar -xzf $src
+      '';
+      installPhase = ''
+        mkdir -p $out/bin/
+        cp ${downloaded-system}/superhtml $out/bin
+      '';
+    };
+  tagged =
+    lib.attrsets.mapAttrs
+    (k: v: mkBinaryInstall {inherit (v.${system}) version url hash downloaded-system;})
+    sources;
+  latest = lib.lists.last (
+    builtins.sort
+    (x: y: (builtins.compareVersions x y) < 0)
+    (builtins.attrNames tagged)
+  );
+in
+  tagged // {"default" = tagged.${latest};}
--- flake.nix
+++ flake.nix
@@ -9,42 +9,9 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: 
-      let
-        version = "0.4.3";
-        convertSystem = system:
-        let
-            jsonString = builtins.readFile ./superhtml.json;
-            jsonData = builtins.fromJSON jsonString;
-            matches = item: item.name == system;
-            matched = builtins.filter matches jsonData;
-            result = if builtins.length matched > 0 then let
-                matchedItem = builtins.head matched;
-            in {
-                system = matchedItem.binary_system;
-                hash = matchedItem.hash;
-            } else abort "Unsupported system ${system}";
-        in
-            result;
-        converted = convertSystem system;
-        superhtml-binary = pkgs.stdenvNoCC.mkDerivation {
-            name = "superhtml";
-            inherit version;
-            src = pkgs.fetchurl {
-                url = "https://github.com/kristoff-it/superhtml/releases/download/v${version}/${converted.system}.tar.gz";
-                sha256 = converted.hash;
-            };
-            unpackPhase = ''
-                tar -xzf $src
-            '';
-            installPhase = ''
-                mkdir -p $out/bin
-                cp ${converted.system}/superhtml $out/bin
-            '';
-        };
-
-      in{
-        packages.default = superhtml-binary;
+      perSystem = { pkgs, system, ... }: 
+      {
+        packages = import ./default.nix {inherit system pkgs;};
         devShells = {
             default = pkgs.mkShell {
                 buildInputs = with pkgs; [python3 ];

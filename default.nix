faa2cbfcbaf9ab264c47dc4a572d9517a0a3f635
Iain H <iain@spiralpoint.org>
2025-12-27 21:02:30 -0500

    Update to use my git fork rather than a release

--- default.nix
+++ default.nix
@@ -1,45 +1,22 @@
 {
   pkgs ? import <nixpkgs> { },
   system ? builtins.currentSystem,
+  superhtml,
 }:
 let
   inherit (pkgs) lib;
-  sources = builtins.fromJSON (builtins.readFile ./superhtml.json);
-  mkBinaryInstall =
-    {
-      url,
-      version,
-      hash,
-      downloaded-system,
-    }:
-    pkgs.stdenvNoCC.mkDerivation {
-      name = "superhtml-${version}";
-      inherit version;
-      src = pkgs.fetchurl {
-        inherit url;
-        sha256 = hash;
-      };
-      unpackPhase = ''
-        tar -xf $src
-      '';
-      installPhase = ''
-        mkdir -p $out/bin/
-        cp superhtml $out/bin
-      '';
-    };
-  tagged = lib.attrsets.mapAttrs (
-    k: v:
-    mkBinaryInstall {
-      inherit (v.${system})
-        version
-        url
-        hash
-        downloaded-system
-        ;
-    }
-  ) sources;
-  latest = lib.lists.last (
-    builtins.sort (x: y: (builtins.compareVersions x y) < 0) (builtins.attrNames tagged)
-  );
 in
-tagged // { "default" = tagged.${latest}; }
+{
+  "default" = pkgs.stdenv.mkDerivation {
+    name = "superhtml";
+    src = superhtml;
+    nativeBuildInputs = with pkgs; [ zig ];
+    buildPhase = ''
+      export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+      zig build -Doptimize=ReleaseSafe --prefix $out
+    '';
+    installPhase = ''
+      true
+    '';
+  };
+}
--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    superhtml.url = "github:iainh/superhtml";
+    superhtml.flake = false;
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, superhtml, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { pkgs, system, ... }: 
       {
-        packages = import ./default.nix {inherit system pkgs;};
+        packages = import ./default.nix {inherit system pkgs superhtml;};
         devShells = {
             default = pkgs.mkShell {
                 buildInputs = with pkgs; [python3 ];
